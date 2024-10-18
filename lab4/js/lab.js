6695df36826cf3ae67349a2fee7c6d1228f78844
ellawynnbot <ella@the-wynns.com>
2024-10-17 22:18:24 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Tic-Tac-Toe
+// Author: Ella Wynn
+// Date: 17 October 2024
 
 // Constants
 
@@ -8,9 +8,30 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
+  //Print empty 3x3 board
+  //Ask for player input for X
+    //If position is not filled 
+    //Place user input 
+    //Else
+  //Output "user input invalid"
+  //Check for tiktactoe
+  //Update board with user input
+  //Computer selects random number
+    //Run valid position loop again
+  //Run tiktactoe check
+  //Repeat until winner
+    //If true
+      //Congrats you won
+    //Else 
+      //Continue game
   // return results;
 }
+
+function checkTiktaktoe(){
+
+}
+
+
 
 function main() {
   console.log("Main function started.");
