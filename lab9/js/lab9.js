e82f29b427ec01d8c167662bf63e91c19569733c
ellawynnbot <ella@the-wynns.com>
2024-11-04 20:47:04 -0800

    lab9

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -13,17 +13,17 @@ $("#problems").append("<button id='button-problems'>Make Special</button>");
 $("#results").append("<button id='button-results'>Make Special</button>");
 
 // Add a click listener to the challenge button
-$("#button-challenge").click(function(){
+$("button-challenge").click(function(){
     // Toggle "special" class on the challenge section
     $("#challenge").toggleClass("special");
 });
 
-$("#button-problems").click(function(){
+$("button-problems").click(function(){
     // Toggle "special" class on the problems section
     $("#problems").toggleClass("special");
 });
 
-$("#button-results").click(function(){
+$("button-results").click(function(){
     // Toggle "special" class on the output section
     $("#results").toggleClass("special");
 });
